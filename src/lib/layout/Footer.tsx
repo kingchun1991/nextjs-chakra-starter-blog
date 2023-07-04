b6b992fa26e4e65a4d0a92cbd7d190130e224582
Frank O <kingchun1991@hotmail.com>
2023-07-04 21:59:04 +0800

    feat: nextseo

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -5,8 +5,12 @@ const Footer = () => {
     <Flex as="footer" width="full" justifyContent="center">
       <Text fontSize="sm">
         {new Date().getFullYear()} -{' '}
-        <Link href="https://sznm.dev" isExternal rel="noopener noreferrer">
-          sznm.dev
+        <Link
+          href="https://nextjs-chakra-mdx.vercel.app/"
+          isExternal
+          rel="noopener noreferrer"
+        >
+          nextjs-chakra-mdx
         </Link>
       </Text>
     </Flex>
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,23 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
+import { NextSeo } from 'next-seo';
 
 import { getFiles, getFileBySlug } from '../../data/mdx';
 import BlogLayout from '../../lib/components/blog';
 import MDXComponents from '../../lib/components/MDXComponents';
-import type { IPosts } from '../../lib/types/custom-types';
+// import type { IPosts } from '../../lib/types/custom-types';
 
 export default function Blog({
   mdxSource,
   post,
 }: {
   mdxSource: any;
-  post: IPosts;
+  post: any;
 }) {
   return (
-    <BlogLayout post={post}>
-      <MDXRemote {...mdxSource} components={MDXComponents} />
-    </BlogLayout>
+    <>
+      <NextSeo
+        title={post.metaInformation.title}
+        description=""
+        openGraph={{
+          type: 'website',
+          locale: 'en',
+          url: `https://nextjs-chakra-mdx.vercel.app/blog/${post.metaInformation.slug}`,
+          title: `${post.metaInformation.title}`,
+          description: '',
+        }}
+      />
+      <BlogLayout post={post}>
+        <MDXRemote {...mdxSource} components={MDXComponents} />
+      </BlogLayout>
+    </>
   );
 }
 
