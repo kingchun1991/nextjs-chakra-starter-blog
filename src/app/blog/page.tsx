446be92d37dfe2b0e0e4e9e99bb41fc6b64ff734
Frank O <kingchun1991@hotmail.com>
2023-07-17 13:54:21 +0800

    feat: static metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,6 +6,18 @@ import BlogPostLayout from '~/lib/layout/BlogPostLayout';
 
 export const metadata: Metadata = {
   title: 'Blog',
+  description:
+    'The is the description of blog from Static Metadata: https://nextjs.org/docs/app/building-your-application/optimizing/metadata#default-fields',
+  openGraph: {
+    url: 'https://nextjs-chakra-mdx.vercel.app/blog',
+    title: 'Blog | nextjs-chakra-mdx',
+    description:
+      'The is the description of blog from Static Metadata: https://nextjs.org/docs/app/building-your-application/optimizing/metadata#default-fields',
+    images: {
+      url: 'https://og-image.sznm.dev/Blog.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg',
+      alt: 'nextjs-chakra-mdx og-image',
+    },
+  },
 };
 
 async function getData() {
