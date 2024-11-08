3bbae4342b454bf1974efb6b5bd1bc7fbe002c0f
Frank O <kingchun1991@hotmail.com>
2024-11-08 16:02:46 +0800

    feat: simplify RSS feed title

--- scripts/jsonGenerator.js
+++ scripts/jsonGenerator.js
@@ -13,7 +13,7 @@ const RSS_FOLDER = './public';
 //     : 'http://localhost:3000';
 const site_url = 'https://nextjs-chakra-starter-blog.vercel.app/';
 const feedOptions = {
-  title: 'Latest Blog Posts | NextJS ChakraUI Starter Blog',
+  title: 'NextJS ChakraUI Starter Blog',
   description: 'feedId:75864871994540032+userId:57276625819860992',
   site_url: site_url,
   feed_url: `${site_url}rss.xml`,
